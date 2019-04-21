60fd67fa7c800266f8fb36d16e460426cbbfc3f5
Arthur Cendrier <arthur.cendrier@epitech.eu>
2019-04-21 13:00:12 +0200

    Make bot message public to all members

--- app/routes.js
+++ app/routes.js
@@ -4,11 +4,18 @@ const Gun = require('./gun');
 
 const gun = new Gun();
 
+const formatAnswer = answer => {
+  return {
+    response_type: 'in_channel',
+    text: answer
+  };
+};
+
 router
   // load a slot with a bullet
   .post('/load', (req, res, next) => {
     const answer = gun.load() ? 'Well locked and loaded' : 'Already full';
-    res.status(200).send(answer);
+    res.status(200).send(formatAnswer(answer));
   })
   // rolls the barrel
   // .post('/roll', (req, res, next) => {
@@ -20,7 +27,7 @@ router
     if (gun.count() === 0) answer += ':warning: _the gun is empty_\n';
     answer += ':gun: ';
     answer += gun.shoot() ? 'BANG' : 'click';
-    res.status(200).send(answer);
+    res.status(200).send(formatAnswer(answer));
   });
 
 module.exports = router;
